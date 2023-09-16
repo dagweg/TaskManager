8d170b4310ae54755db4f2d7f4bc1cf7d1ba3579
dagweg <dagtef2@gmail.com>
2023-09-16 13:56:53 +0300

    Features Added

--- src/TaskManager/taskmanager.js
+++ src/TaskManager/taskmanager.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './taskmanager.css'
+import Footer from "./footer";
+import TaskPane from "./taskpane";
 
 export default function TaskManager() {
   return (
@@ -10,16 +12,10 @@ export default function TaskManager() {
         </div>
         <div className="main-panel">
           <button id="new-task">Add new task</button>
+          <TaskPane />
         </div>
       </div>
-      <div className="foot-panel">
-        <div className="contact">
-          <a href="https://www.linkedin.com/in/dagmawi-wegayehu-6a2057216/" id="in-contact" target="_blank"></a>
-          <a href="https://github.com/dagweg" id="git-contact" target="_blank"></a>
-          <a href="https://www.instagram.com/dagweg7/" id="insta-contact" target="_blank"></a>
-        </div>
-        <h4>All rights reserved 2023©</h4>
-      </div>
+      <Footer />
     </>
   );
 }
